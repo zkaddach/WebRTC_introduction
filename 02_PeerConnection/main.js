de84293f99c626d0a267bb62a4f5a728c99648dd
Zakaria <kaddachezakaria@gmail.com>
2021-03-10 14:36:27 +0100

    [fix] refactor l'ordre des blocs de code

--- 02_PeerConnection/main.js
+++ 02_PeerConnection/main.js
@@ -9,9 +9,18 @@ function handleError(err){
     console.log(err);
 }
 
-var mediaOptions = {
-    video: true,
-    audio: true
+/**
+ * Méthode fictive permettant d'envoyer l'offre au pair distant.
+ */
+function sendOfferToRemotePc(offer) {
+  receivedOfferFromLocalPc(offer);
+}
+
+/**
+ * Méthode fictive permettant d'envoyer la réponse au pair distant.
+ */
+function sendAnswerToLocalPc(answer) {
+    receivedAnswerFromRemotePc(answer);
 }
 
 // L'objet RTCPeerConnection a besoin des ICE servers (nous expliquerons ca) par la suite.
@@ -66,40 +75,36 @@ remotePc.onaddstream = function (e){
 }
 
 // On récupère les flux video et audio local
+var mediaOptions = {
+    video: true,
+    audio: true
+}
 navigator.mediaDevices.getUserMedia(mediaOptions)
     .then(shareStream)
     .catch(handleError)
 
-/**
- * Méthode permettant d'envoyer l'offre au pair distant.
- */
-function sendOfferToRemotePc(offer) {
-  receivedOfferFromLocalPc(offer);
-}
+
 
 /**
- * Méthode permettant de recevoir l'offre du pair distant.
+ * Méthode permettant de recevoir l'offre du pair appelant.
  */
 function receivedOfferFromLocalPc(offer) {
+  // Utilisation de la description reçue
   remotePc.setRemoteDescription(offer);
   /**
-   * Suite à cela le pair distant doit creer une réponse et renvoyer également
-   * la description.
+   * Suite à cela le pair appelé doit creer une réponse et renvoyer également
+   * sa description.
    */
   remotePc.createAnswer()
       .then((desc) => {
+          // Utilisation de sa propre description
           remotePc.setLocalDescription(desc);
+          // Envoie de la réponse au pair appelant
           sendAnswerToLocalPc(desc);
 
       })
 }
 
-/**
- * Méthode permettant d'envoyer la réponse au pair distant.
- */
-function sendAnswerToLocalPc(answer) {
-    receivedAnswerFromRemotePc(answer);
-}
 
 /**
  * Méthode permettant de recevoir la réponse du pair distant.
@@ -111,18 +116,17 @@ function receivedAnswerFromRemotePc(answer) {
 // Losqu'on souhaite effectuer la connexion entre nos deux objets RTCPeerConnection
 document.getElementById("call").onclick = function () {
 
-  // On crée les options de l'offre a envoyer
-    var offerOptions = {
-        offerToReceiveAudio: 1,
-        offerToReceiveVideo: 1
-    };
     /**
      * La creation de l'offre necessite de definir au moins la "Description"
      * de la connexion (encodage, compression, algorithmes de cryptage, etc)
      */
+     var offerOptions = {
+         offerToReceiveAudio: 1,
+         offerToReceiveVideo: 1
+     };
     localPc.createOffer(offerOptions)
         .then((desc) => {
-            console.log(desc);
+            console.log("Description de la session lors de la création de l'offre : ", desc);
             localPc.setLocalDescription(desc);
             sendOfferToRemotePc(desc);
 
